33509a97f6046980c225f794fb4a991bc74445ab
Shoval Tayro <57803367+ShovalTayro@users.noreply.github.com>
2021-03-03 13:17:38 +0200

    fix the save to file

--- doc2vec_new.py
+++ doc2vec_new.py
@@ -1,13 +1,9 @@
 import gensim
 import pandas as pd
 
-
-
 df = pd.read_excel('first_300_des_res.xlsx', engine='openpyxl')
 docLabels = df['city'].tolist()
 data= df['description'].tolist()
-
-
 
 class LabeledLineSentence(object):
       def __init__(self, doc_list, labels_list):
@@ -18,15 +14,13 @@ class LabeledLineSentence(object):
           for idx, doc in enumerate(self.doc_list):
               yield gensim.models.doc2vec.TaggedDocument(doc,
                                                           [self.labels_list[idx]])
+
 # iterator returned over all documents
 it = LabeledLineSentence(data, docLabels)
-
-
-
-model = gensim.models.Doc2Vec(vector_size=300, min_count=0, alpha=0.025, min_alpha=0.025)
+model = gensim.models.Doc2Vec(vector_size=500, min_count=0, alpha=0.025, min_alpha=0.025)
 model.build_vocab(it)
 #training of model
-for epoch in range(10):
+for epoch in range(100):
  print("iteration"+str(epoch+1))
  model.train(it,total_examples=model.corpus_count,epochs=model.epochs)
  model.alpha -= 0.002
@@ -36,17 +30,15 @@ for epoch in range(10):
 model.save('doc2vec.model')
 print("model saved")
 
-
 #loading the model
 d2v_model = gensim.models.doc2vec.Doc2Vec.load("doc2vec.model")
 
-
 # write the result to txt
-f = open("10most_similarities.txt", "a")
-for city in range(2):
+f = open("10most_similarities.txt", "w+", errors="ignore")
+for city in range(len(docLabels)):
     similar_doc = d2v_model.docvecs.most_similar(docLabels[city])
     # for x in similar_doc:
-    f.write('\n'.join('{} {}'.format(x[0], x[1]) for x in similar_doc))
+    sentence = ', '.join('{} {}'.format(x[0], x[1]) for x in similar_doc)
+    out = docLabels[city] + "= " + sentence
+    f.write(out + '\n')
 f.close()
-
-
