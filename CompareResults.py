b728aa08961305265654346ec51b88ba8d78dd6b
Shoval Tayro <57803367+ShovalTayro@users.noreply.github.com>
2021-05-26 17:23:15 +0300

    add history search

--- CompareResults.py
+++ CompareResults.py
@@ -4,7 +4,7 @@ import imgsimop1
 import imagesimop2
 import imagesimop3
 import image2vec
-
+import historySearch
 cities = ['Antalya', 'Bangkok', 'Beijing', 'Crete',
           'Delhi', 'Hong-Kong', 'Los-Angeles', 'Mumbai-Bombay',
           'New-York-City', 'Paris', 'Rhodes', 'Rome']
@@ -17,12 +17,14 @@ image_option1 = imgsimop1.image_option1_similarity()
 image_option2 = imagesimop2.image_option2_similarity()
 image_option3 = imagesimop3.image_option3_similarity()
 image_our_option = image2vec.image_our_option_similarity()
+history_search = historySearch.history_search_similarity()
 
 text_dif = 0
 image_option1_dif = 0
 image_option2_dif = 0
 image_option3_dif = 0
 image_our_option_dif = 0
+history_search_dif = 0
 for i in cities:
     for j in cities:
         text_dif += abs(text_similarity.loc[i, j] - AMT_result.loc[i, j])
@@ -30,13 +32,15 @@ for i in cities:
         image_option2_dif += abs(image_option2.loc[i, j] - AMT_result.loc[i, j])
         image_option3_dif += abs(image_option3.loc[i, j] - AMT_result.loc[i, j])
         image_our_option_dif += abs(image_our_option.loc[i, j] - AMT_result.loc[i, j])
+        history_search_dif += abs(history_search.loc[i, j] - AMT_result.loc[i, j])
 
 print("text =", text_dif)
 print("image option 1 =", image_option1_dif)
 print("image option 2 =", image_option2_dif)
 print("image option 3 =", image_option3_dif)
 print("image our option =", image_our_option_dif)
-min = min(text_dif, image_option1_dif, image_option2_dif, image_option3_dif, image_our_option_dif)
+print("history search =", history_search_dif)
+min = min(text_dif, image_option1_dif, image_option2_dif, image_option3_dif, image_our_option_dif, history_search_dif)
 winner = ""
 if min == text_dif:
     winner = "text similarity"
@@ -46,8 +50,10 @@ elif min == image_option2_dif:
     winner = "image option 2"
 elif min == image_option3_dif:
     winner = "image option 3"
-else:
+elif min == image_our_option_dif:
     winner = "image our option"
+else:
+    winner = "history search"
 print("in distance between matrices: the must represent method is :", winner)
 
 
---
+++ historySearch.py
@@ -0,0 +1,44 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+from sklearn.preprocessing import minmax_scale
+
+
+def history_search_similarity():
+    similarity_heatmap_data = pd.DataFrame()
+    df = pd.read_excel('DistinctCityDestNet.xlsx', engine='openpyxl')
+    cities = ['Antalya', 'Bangkok', 'Beijing', 'Crete',
+              'Delhi', 'Hong-Kong', 'Los-Angeles', 'Mumbai-Bombay',
+              'New-York-City', 'Paris', 'Rhodes', 'Rome']
+    N = df['N'].tolist()
+    res = []
+    index = 0
+    for i in range(len(cities)):
+        for j in range(len(cities)):
+            res.append(math.log(N[index] + 1))
+            index = index + 1
+
+    index = 0
+    normalized_res = minmax_scale(res)
+    for i in range(len(cities)):
+        for j in range(len(cities)):
+            similarity_heatmap_data = similarity_heatmap_data.append(
+                {
+                    'similarity': normalized_res[index],
+                    'city1': cities[i],
+                    'city2': cities[j]
+                },
+                ignore_index=True
+            )
+            index = index + 1
+    similarity_heatmap = similarity_heatmap_data.pivot(index="city1", columns="city2", values="similarity")
+    ax = sns.heatmap(similarity_heatmap, cmap="YlGnBu", annot=True, annot_kws={'size': 8})
+    plt.title("History Search Similarity")
+    for label1 in ax.get_yticklabels():
+        label1.set_weight('bold')
+    for label2 in ax.get_xticklabels():
+        label2.set_weight('bold')
+    plt.show()
+    return similarity_heatmap
